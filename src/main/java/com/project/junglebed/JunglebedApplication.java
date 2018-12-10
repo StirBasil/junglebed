93aa501688b029e0302f9a4d97c05fd3f473476b
PratchayagonHomkum <pratchayagon.3707@gmail.com>
2018-12-10 15:21:21 +0700

    Initial commit

---
+++ src/main/java/com/project/junglebed/JunglebedApplication.java
@@ -0,0 +1,12 @@
+package com.project.junglebed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunglebedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunglebedApplication.class, args);
+	}
+}
