e1f0f5555269e52b16d9c046f294948a9e2c0815
PratchayagonHomkum <pratchayagon.3707@gmail.com>
2018-12-10 15:48:25 +0700

    2

--- src/main/java/com/project/junglebed/JunglebedController.java
+++ src/main/java/com/project/junglebed/JunglebedController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class JunglebedController {
 
-    @GetMapping
+    @GetMapping("/")
     public String index(){
         return "Welcome JungleBed API";
     }
