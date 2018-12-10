56cee06d9f117383d6032b983dcc8c9856c9100b
PratchayagonHomkum <pratchayagon.3707@gmail.com>
2018-12-10 16:27:11 +0700

    3

--- src/main/java/com/project/junglebed/JunglebedController.java
+++ src/main/java/com/project/junglebed/JunglebedController.java
@@ -1,6 +1,7 @@
 package com.project.junglebed;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,10 @@ public class JunglebedController {
     @GetMapping("/")
     public String index(){
         return "Welcome JungleBed API";
+    }
+
+    @GetMapping("/authen")
+    public String authen(@RequestParam(defaultValue = "") String username,@RequestParam(defaultValue = "") String password){
+        return username + " , " + password;
     }
 }
